bc21de0b3a49a8faf3d8bba686e0edf0f3df48a0
Jason Pearson <jasonp.pdx@gmail.com>
2016-06-02 15:31:05 -0400

    open/close animation for sliders

--- js/core-values.js
+++ js/core-values.js
@@ -3,23 +3,59 @@ import Flickity from 'flickity'
 export class CoreValues {
 
   constructor() {
-    this.slider = document.querySelector('.core-values-slider')    
+    this.slider = document.querySelector('.core-values-slider')
+    this.cells = this.slider.childNodes
+    this.activeCell = null     
+    const instance = this
+        
+    $(this.cells).on('mouseenter', function() { $(this.cells).not(this).addClass('sibling-is-hovered') })
+    $(this.cells).on('mouseleave', function() { $(this.cells).removeClass('sibling-is-hovered') })
+    $(this.cells).on('click', function() { instance.openCell(this) })
+    $('span.close-cell').on('click', function(e) { e.stopPropagation(); instance.closeCell() })
+  }
+  
+  openCell(cell) {
+    if (!this.activeCell) {
+
+      $(this.cells).not(cell).addClass('sibling-is-opening')
+      $(cell).addClass('opening')
+      
+      setTimeout(() => {        
+        $(this.slider).removeClass('uninitialized')        
+        $(this.cells).removeClass('sibling-is-opening opening')
+                        
+        this.initFlkty( $(cell).index() )
+        this.activeCell = cell
+      }, 800)
+
+    }
+  }  
+  
+  closeCell() {
+    if (this.activeCell) {              
+
+      this.flkty.destroy()
+      
+      $(this.cells).not(this.activeCell).addClass('sibling-is-opening')
+      $(this.activeCell).addClass('opening')
+      $(this.slider).addClass('uninitialized')
+
+      setTimeout(() => {
+        $(this.cells).removeClass('sibling-is-opening')
+        $(this.activeCell).removeClass('opening')
+        this.activeCell = null        
+      })      
+    
+    }
+  }
+  
+  initFlkty(initialIndex) {
     this.flkty = new Flickity( this.slider, {
       cellAlign: 'left',
       contain: true,
-      // wrapAround: true      
+      initialIndex: initialIndex      
     })
-    
-    this.flkty.on('staticClick', () => {
-      const isFullWidth = this.slider.classList.value.includes('full-width')
-      if (isFullWidth) {
-        this.slider.classList.remove('full-width')
-      } else {
-        this.slider.classList.add('full-width')  
-      }
-      
-      this.flkty.resize()
-    })      
-  }
+        
+  } 
 
 }
