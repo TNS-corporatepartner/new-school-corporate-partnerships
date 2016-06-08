2864703f331bbf280a78f98ef61131b09aa8c867
Jason Pearson <jasonp.pdx@gmail.com>
2016-06-08 16:00:12 -0400

    initially animation wip

--- js/future-of.js
+++ js/future-of.js
@@ -1,5 +1,5 @@
 import Flickity from 'flickity'
-
+import 'd3'
 
 export class FutureOf {  
   
@@ -11,63 +11,265 @@ export class FutureOf {
     this.loadingEl = document.querySelector('.shuffler')
     this.loadingWord = this.loadingEl.querySelector('h1')     
     this.questionEl = document.querySelector('.question')  
+    this.slider = document.querySelector('.future-of-slider')
+
     this.questions = [
       'How can data be human?',
       'How is the gamification of learning reshaping the workforce?',
       'question 3'
     ]
+
     this.words = [
       'Big Data',
       'Learning & Development',
       'three'
     ]
 
-
-    var slider = document.querySelector('.future-of-slider')    
-    this.flkty = new Flickity( slider, {
-      cellAlign: 'left',
-      contain: true,
-      wrapAround: true
-    })  
-    
-    // this.playFutureOfSlider( this.futureOfCells[this.activeFutureOfCellIndex] )
-    
-    
-    $('video').each(function(i, vid) {
-      vid.play()
-    }) 
-    
-       
-    const shufflerConfig = {
+    this.shufflerConfig = {
       limit: 26,
       count: 0,
       index: 0,
       words: ['The Workforce', 'Sustainability', 'Research & Development', 'Big Data']   
-    }      
-            
-    Velocity(this.imagineEl, {opacity: 1}, {
-      duration: 1200, display: 'block',
-      complete: () => {
-        Velocity(this.futureOfEl, {opacity: 1}, {
-          duration: 800,
-          display: 'block',
-          complete: () => {
-            this.shuffler(shufflerConfig).then( () => {
-              
-              setTimeout(() => {
-                Velocity(this.imagineEl, {opacity: 0.2})
-                Velocity(this.futureOfEl, {opacity: 0.2}, {
-                  complete: () => showQuestion.call(this) 
-                })
-                Velocity(this.loadingWord, {opacity: 0.2})                
-              }, 1000)
+    }     
+    
+    this.flkty = new Flickity( this.slider, {
+      cellAlign: 'left',
+      contain: true,
+      wrapAround: true
+    })  
 
-            })
-          }
-        })
-      }
+    this.initSplashContent().then(() => {
+
+      this.line1  
+        .transition()
+        .duration(2000)
+        .attr('x2', '100%')
+
+      this.line2        
+        .transition()
+        .duration(2000)
+        .attr('x2', '20%')
+
+      this.svgTextEl      
+        .transition()
+        .duration(2000)
+        .attr('y', -4)
+
     })
+
+
+
+
+
+    // setTimeout(() => {
+    //   this.redRect
+    //     .transition()
+    //     .duration(800)
+    //     .attr('width', '120px')
+    //     .attr('height', '52px')
+    //     .attr('transform', `translate(${window.innerWidth - 120}, 0)`)
+    // }, 3000)
+
+
+    // window.addEventListener('resize', () => {
+    //   console.log(this.introSvg)
+    //   this.introSvg
+    //     .attr('viewBox', `0 0 ${window.innerWidth} ${window.innerHeight}`)
+    //     .attr('width', window.innerWidth + 'px')
+    //     .attr('height', window.innerHeight + 'px')        
+
+    //   // this.redRect      
+    //   //   .attr('width', window.innerWidth)
+    //   //   .attr('height', window.innerHeight)
+    // })
+  }
+
+  initSplashContent() {
+    var paddingLR = (window.innerWidth * 0.10).toFixed(0)
+    var contentWidth = (window.innerWidth * 0.80).toFixed(0)
+    var midY = parseInt( (window.innerHeight / 2).toFixed(0) )
+    var lineY = parseInt( (window.innerHeight / 2).toFixed(0) )
+
+    return new Promise(resolve => {
+
+      d3.xml('/images/tns-logo.svg', 'image/svg+xml', (error, xml) => {
+        if (error) throw error;            
+
+        this.introSvg = d3.select('#futureOf')
+          .append('svg')
+            .attr('id', 'redRect')
+            .attr('viewBox', `0 0 ${window.innerWidth} ${window.innerHeight}`)
+            .attr('preserveAspectRatio', 'xMidYMid slice')
+            .attr('width', window.innerWidth + 'px')
+            .attr('height', window.innerHeight + 'px')
+            .style('position', 'absolute')      
+            .style('top', 0)      
+
+        this.redRect = this.introSvg.append('rect')      
+          .attr('x', 0)
+          .attr('y', 0)
+          .attr('width', window.innerWidth + 'px')
+          .attr('height', window.innerHeight + 'px')
+          .attr('fill', 'red')
+
+        this.redRectContent = this.introSvg.append('svg')
+          .attr('id', '#redRectContent')
+          .attr('x', paddingLR)
+          .attr('y', midY)
+          .attr('width', '80%')
+
+        // this.redRectContent[0][0].appendChild(xml.documentElement)
+      
+        this.redRectContent.append('clipPath')
+          .attr('id', 'clip1')
+          .append('rect')
+            .attr('x', 0)
+            .attr('y', 12)
+            .attr('height', 150)
+            .attr('width', '100%')
+
+        /** DEBUG **/
+        this.redRectContent.append('rect')
+          .attr('stroke', 'yellow')
+          .attr('fill', 'none')      
+          .attr('x', 0)
+          .attr('y', 12)
+          .attr('height', 150)
+          .attr('width', '100%') 
+
+        this.redRectContent.append('rect')
+          .attr('stroke', 'green')
+          .attr('fill', 'none')      
+          .attr('x', 0)
+          .attr('y', 0)
+          .attr('height', 150)
+          .attr('width', '20%') 
+
+        /** END DEBUG **/
+
+        this.svgTextEl = this.redRectContent.append('text')
+          .attr('clip-path', 'url(#clip1)')
+          .attr('x', '20%')
+          .attr('y', -150)
+          .attr('fill', 'white')
+          .attr('font-size', 70)        
+
+        this.svgTextEl.append('tspan').attr('x', '20%').attr('dy', '1.05em').style('font-family', 'Neue')    .text('Corporate')
+        this.svgTextEl.append('tspan').attr('x', '20%').attr('dy', '1.05em').style('font-family', 'Neue')    .text('Partnerships')    
+
+        this.line1 = this.redRectContent
+          .append('line')
+            .attr('stroke-width', '5px')
+            .attr('stroke', 'white')
+            .attr('x1', 0)
+            .attr('y1', 4)
+            .attr('x2', 0)
+            .attr('y2', 4)
+
+        this.line2 = this.redRectContent
+          .append('line')
+            .attr('stroke-width', '5px')
+            .attr('stroke', 'white')
+            .attr('x1', 0)
+            .attr('y1', 14)
+            .attr('x2', 0)
+            .attr('y2', 14)
+      
+        resolve()
+      })
+
+    })
+
+  }
+
+  shuffler(o) {
+    this.loadingEl = document.querySelector('.shuffler')
+    this.loadingWord = this.loadingEl.querySelector('h1')
+    
+    Velocity(this.loadingWord, {opacity: 1}, {duration: 300})
+    
+    return new Promise(resolve => {
+      const wordSwitcher = setInterval(() => {
+        this.loadingWord.textContent = o.words[o.index]
+        o.index = o.index > o.words.length ? 0 : o.index     
         
+        if (o.count === o.limit) {
+          clearInterval(wordSwitcher)
+          this.loadingWord.textContent = o.words[o.words.length - 1]
+          resolve()
+        } else {
+          o.count++
+          o.index++      
+        }
+        
+      }, 100)    
+    })  
+  }
+  
+}
+
+
+    // $('video').each(function(i, vid) {
+    //   vid.play()
+    // }) 
+           
+            
+    // Velocity(this.imagineEl, {opacity: 1}, {
+    //   duration: 1200, display: 'block',
+    //   complete: () => {
+    //     Velocity(this.futureOfEl, {opacity: 1}, {
+    //       duration: 800,
+    //       display: 'block',
+    //       complete: () => {
+    //         this.shuffler(shufflerConfig).then( () => {
+              
+    //           setTimeout(() => {
+    //             Velocity(this.imagineEl, {opacity: 0.2})
+    //             Velocity(this.futureOfEl, {opacity: 0.2}, {
+    //               complete: () => showQuestion.call(this) 
+    //             })
+    //             Velocity(this.loadingWord, {opacity: 0.2})                
+    //           }, 1000)
+
+    //         })
+    //       }
+    //     })
+    //   }
+    // })     
+
+
+
+    // $('video').each(function(i, vid) {
+    //   vid.play()
+    // }) 
+           
+            
+    // Velocity(this.imagineEl, {opacity: 1}, {
+    //   duration: 1200, display: 'block',
+    //   complete: () => {
+    //     Velocity(this.futureOfEl, {opacity: 1}, {
+    //       duration: 800,
+    //       display: 'block',
+    //       complete: () => {
+    //         this.shuffler(shufflerConfig).then( () => {
+              
+    //           setTimeout(() => {
+    //             Velocity(this.imagineEl, {opacity: 0.2})
+    //             Velocity(this.futureOfEl, {opacity: 0.2}, {
+    //               complete: () => showQuestion.call(this) 
+    //             })
+    //             Velocity(this.loadingWord, {opacity: 0.2})                
+    //           }, 1000)
+
+    //         })
+    //       }
+    //     })
+    //   }
+    // })
+
+
+
+
     function showQuestion() {
       this.questionEl.textContent = this.questions[this.flkty.selectedIndex]
       Velocity(this.futureOfCells[0], { opacity: 1}, {
@@ -122,108 +324,3 @@ export class FutureOf {
       })
                         
     }
-  }
- 
-  playFutureOfSlider(cell) {
-    Velocity(this.imagineEl, 'reverse')
-    Velocity(this.futureOfEl, 'reverse')
-    Velocity(this.loadingWord, 'reverse')
-
-    setTimeout(() => {
-      this.playFutureOfCell(cell).then(() => {  
-        if ( ++this.activeFutureOfCellIndex >= this.futureOfCells.length ) {
-          this.activeFutureOfCellIndex = 0
-        }            
-        
-        this.flkty.next(true)      
-        this.playFutureOfSlider( this.futureOfCells[this.activeFutureOfCellIndex] )          
-      })      
-    }, 1500)     
-  }
- 
- 
-  playFutureOfCell(cell) {
-    this.imagineEl = document.querySelector('.future-of-header h1')
-    this.futureOfEl = document.querySelectorAll('.future-of-header h1')[1]    
-    const shufflerConfig = {
-      limit: 26,
-      count: 0,
-      index: 0,
-      words: ['The Workforce', 'Sustainability', 'Research & Development', 'Big Data']   
-    }  
-    
-    return new Promise(resolve => {
-      Velocity(this.imagineEl, {opacity: 1}, {
-        duration: 1200, display: 'block',
-        complete: () => {
-          Velocity(this.futureOfEl, {opacity: 1}, {
-            duration: 800,
-            display: 'block',
-            complete: () => {
-              this.shuffler(shufflerConfig).then( () => {
-                const video = cell.querySelector('video')             
-                
-                setTimeout(video.play.bind(video), 200)              
-
-                Velocity( cell, {opacity: 1}, {
-                  duration: 1000,
-                  display: 'block',
-                  complete: () => playVideo(video).then(() => {
-                    Velocity(cell, 'reverse')
-                    setTimeout(resolve, 200)
-                  })
-                })              
-              })             
-            }
-          })
-        }
-      })  
-    })
-    
-    function playVideo(video) {
-      const limit = 2
-      let count = 0
-      let nextCalled = false
-      
-      video.addEventListener('ended', () => { if (++count < limit) video.play() })
-      
-      return new Promise(resolve => {            
-        video.addEventListener('timeupdate', function checkProgress() {        
-          const percentComplete = video.currentTime / video.duration * 100
-          
-          if (count + 1 == limit && percentComplete >= 50 && !nextCalled) {
-            video.removeEventListener(video, checkProgress)
-            nextCalled = true
-            resolve()
-          }      
-        })           
-      })              
-    }   
-  }
-
-
-  shuffler(o) {
-    this.loadingEl = document.querySelector('.shuffler')
-    this.loadingWord = this.loadingEl.querySelector('h1')
-    
-    Velocity(this.loadingWord, {opacity: 1}, {duration: 300})
-    
-    return new Promise(resolve => {
-      const wordSwitcher = setInterval(() => {
-        this.loadingWord.textContent = o.words[o.index]
-        o.index = o.index > o.words.length ? 0 : o.index     
-        
-        if (o.count === o.limit) {
-          clearInterval(wordSwitcher)
-          this.loadingWord.textContent = o.words[o.words.length - 1]
-          resolve()
-        } else {
-          o.count++
-          o.index++      
-        }
-        
-      }, 100)    
-    })  
-  }
-  
-}
