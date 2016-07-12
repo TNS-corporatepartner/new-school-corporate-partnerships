e61c4f87a5b62561c4b438236f26dee56e4ce508
Keith Mancuso <keithmancuso@gmail.com>
2016-07-12 19:36:25 -0400

    remove partner with us buttons

--- bundle.js
+++ bundle.js
@@ -14569,6 +14569,31 @@
 	// shim for using process in browser
 
 	var process = module.exports = {};
+
+	// cached from whatever global is present so that test runners that stub it
+	// don't break things.  But we need to wrap it in a try catch in case it is
+	// wrapped in strict mode code which doesn't define any globals.  It's inside a
+	// function because try/catches deoptimize in certain engines.
+
+	var cachedSetTimeout;
+	var cachedClearTimeout;
+
+	(function () {
+	  try {
+	    cachedSetTimeout = setTimeout;
+	  } catch (e) {
+	    cachedSetTimeout = function () {
+	      throw new Error('setTimeout is not defined');
+	    }
+	  }
+	  try {
+	    cachedClearTimeout = clearTimeout;
+	  } catch (e) {
+	    cachedClearTimeout = function () {
+	      throw new Error('clearTimeout is not defined');
+	    }
+	  }
+	} ())
 	var queue = [];
 	var draining = false;
 	var currentQueue;
@@ -14593,7 +14618,7 @@
 	    if (draining) {
 	        return;
 	    }
-	    var timeout = setTimeout(cleanUpNextTick);
+	    var timeout = cachedSetTimeout(cleanUpNextTick);
 	    draining = true;
 
 	    var len = queue.length;
@@ -14610,7 +14635,7 @@
 	    }
 	    currentQueue = null;
 	    draining = false;
-	    clearTimeout(timeout);
+	    cachedClearTimeout(timeout);
 	}
 
 	process.nextTick = function (fun) {
@@ -14622,7 +14647,7 @@
 	    }
 	    queue.push(new Item(fun, args));
 	    if (queue.length === 1 && !draining) {
-	        setTimeout(drainQueue, 0);
+	        cachedSetTimeout(drainQueue, 0);
 	    }
 	};
 
@@ -32623,20 +32648,11 @@
 	      $('#ourPeople').one('click', function (e) {
 	        e.stopPropagation();
 
-	        tl.timeScale(0.45);
+	        tl.timeScale(0.25);
 
 	        $('#ourPeople').removeClass('modal-open');
 	        modalContent.innerHTML = '';
 	      });
-	    });
-
-	    $('.person.video').on('mouseenter', function () {
-	      _this.tl.timeScale(0.09);
-	    });
-
-	    $('.person.video').on('mouseleave', function () {
-	      console.log('leave');
-	      _this.tl.timeScale(0.45);
 	    });
 
 	    this.tl.add(new TweenMax(this.sliderInner, '5', {
@@ -32645,10 +32661,10 @@
 	      timeScale: this.timeScale
 	    }));
 
-	    this.tl.timeScale(0.45);
+	    this.tl.timeScale(0.25);
 
 	    var mouseleave$ = _rxjs.Observable.fromEvent(this.slider, 'mouseleave').subscribe(function () {
-	      _this.tl.timeScale(0.45);
+	      _this.tl.timeScale(0.25);
 	    });
 
 	    var mousemove$ = _rxjs.Observable.fromEvent(this.slider, 'mousemove').map(function (e) {
@@ -32660,7 +32676,7 @@
 	        _this.tl.reversed(true);
 	      }
 
-	      //this.tl.timeScale( Math.abs(e.x / 50) )
+	      _this.tl.timeScale(Math.abs(e.x / 15));
 	    });
 	  }
 
