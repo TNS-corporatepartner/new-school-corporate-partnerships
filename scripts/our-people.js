9939c7eba242343c731e4f906c33ce2d407a2da7
Jason Pearson <jasonp.pdx@gmail.com>
2016-09-07 11:50:18 -0400

    iOS html5 video instead of vimeo

--- scripts/our-people.js
+++ scripts/our-people.js
@@ -12,38 +12,9 @@ export class OurPeople {
     this.section = document.getElementById('ourPeople')
     this.sectionHeadlines = this.section.querySelector('.section-headlines')
     this.sectionIntro = this.section.querySelector('.section-intro')
-
-    setTimeout(() => {
-      $(this.sectionIntro).addClass('hidden')
-      
-      setTimeout(() => {
-        $(this.sectionHeadlines).removeClass('hidden')
-      }, 1800)      
-    }, 750)
-
     this.slider = document.getElementById('peopleSlider')
     this.sliderInner = this.slider.querySelector('.slider-inner')
-    this.walkDirection = 'right'
-    this.lastLeft = 0
-    this.tl = new TimelineMax({
-      repeat: -1,
-      smoothChildTiming: true,
-      onReverseComplete: () => {
-        this.tl.seek('5')
-      }
-    })
 
-    this.timeScale = 5
-    
-    new Hammer(this.slider, {threshold: 10}).on('pan', (e) => {
-      const direction = e.velocityX * -1 >= 0 ? 'right' : 'left"'
-      this.tl.timeScale(e.velocityX / -2)
-
-      this.timeScaleTween && this.timeScaleTween.kill()
-      const settleSpeed = direction === 'right' ? 0.05 : -0.05
-      this.timeScaleTween = TweenMax.to(tl, 1.75, {timeScale: settleSpeed})      
-    })
-    
 
     Array.prototype.forEach.call(document.querySelectorAll('.grid'), (chunk) => {
       new Isotope( chunk, {
@@ -57,34 +28,131 @@ export class OurPeople {
     this.sliderInner.style.width = sliderInnerWidth + 'px'
     this.cellWidth = this.slider.querySelector('.cell').offsetWidth
 
+    this.tl = new TimelineMax({
+      repeat: -1,
+      smoothChildTiming: true,
+      onReverseComplete: () => {
+        this.tl.seek('5')
+      }
+    })
+
+    const tl = this.tl    
+
+    this.tl.add( new TweenMax(this.sliderInner, '5', {
+      left: this.cellWidth * -1,
+      ease: Linear.easeNone,
+      // timeScale: 5
+    }))
+
+    this.tl.timeScale(0.05)
+
+    this.hammer = new Hammer(this.slider, {threshold: 10})
+    
+    this.hammer.on('pan', (e) => {
+      const direction = e.velocityX * -1 >= 0 ? 'right' : 'left"'
+      this.tl.timeScale(e.velocityX / -2)
+
+      this.timeScaleTween && this.timeScaleTween.kill()
+      const settleSpeed = direction === 'right' ? 0.05 : -0.05
+      this.timeScaleTween = TweenMax.to(tl, 1.75, {timeScale: settleSpeed})      
+    })
+
+    // this.hammer.on('tap', (e) => {
+    //   console.log(e)
+    // })
+
     $('#peopleSlider').removeClass('hidden')
 
-    const tl = this.tl
+    setTimeout(() => {
+      $(this.sectionIntro).addClass('hidden')
+      
+      setTimeout(() => {
+        $(this.sectionHeadlines).removeClass('hidden')
+      }, 1800)
+    }, 750)
 
-    // $('.person.video').on('click', function(e) {
-    //   e.stopPropagation()
 
-    //   tl.pause()
+    if (Modernizr.touchevents) {
+      this.videos = this.slider.querySelectorAll('video')
+      this.overlays = this.slider.querySelectorAll('.person.video .overlay')
 
-    //   const personModal = document.getElementById('personModal')
-    //   const modalContent = personModal.querySelector('.content')
-    //   const videoSrc = `https://player.vimeo.com/video/${$(this).data('src')}?autoplay=1&color=e82e21&title=0&byline=0&portrait=0`;
-    //   const video = $('<iframe frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
+      Array.prototype.forEach.call(this.overlays, overlay => {
+        new Hammer(overlay).on('tap', function(e) {
+          this.timeScaleTween && this.timeScaleTween.kill()
+          this.timeScaleTween = TweenMax.to(tl, 0.5, {timeScale: 0})
 
-    //   video.attr('src', videoSrc)
-    //   video.attr('autoplay', true)
+          const video = e.target.parentNode.children[0]          
+          video.play()
+        })
+      })
 
-    //   $('#ourPeople').addClass('modal-open');
-    //   $('body').addClass('people-modal-open')
+      Array.prototype.forEach.call(this.videos, vid => {
+        vid.addEventListener('webkitendfullscreen', (e) => {
+          console.log(e.currentTarget)
+          console.log(this)
+          e.currentTarget.pause()        
+          e.currentTarget.currentTime = 0
 
-    //   $(modalContent).append(video)
+          this.timeScaleTween && this.timeScaleTween.kill()
+          this.timeScaleTween = TweenMax.to(tl, 1.75, {timeScale: 0.05})          
+        })
+      })
 
-    //   if (window.innerWidth >= 700) {
-    //     $('#ourPeople').one('click', closeVideo)        
-    //   } else {
-    //     $('#closePersonModal').one('click', closeVideo)
-    //   }
-    // })
+    } else {
+      $('.person.video').on('mouseenter', () => {
+        this.timeScaleTween && this.timeScaleTween.kill()
+        this.timeScaleTween = TweenMax.to(tl, 1.75, {timeScale: 0})
+      })
+
+      $('.person.video').on('mouseleave', () => {
+        this.timeScaleTween && this.timeScaleTween.kill()
+        this.timeScaleTween = TweenMax.to(tl, 1.75, {timeScale: 0.05})
+      })
+
+      $('.hover-area').on('mouseenter', (e) => {      
+        const isScrollingForward = e.currentTarget.id === 'hoverRight'
+        const isReversed = this.tl.reversed()
+
+        if (isScrollingForward && isReversed) {
+          this.tl.reversed(false)
+        } else if( !isScrollingForward && !isReversed ) {
+          this.tl.reverse()
+        }
+
+        this.timeScaleTween && this.timeScaleTween.kill()
+        this.timeScaleTween = TweenMax.to(this.tl, 2, {timeScale: 1})
+      });
+
+      $('.hover-area').on('mouseleave', () => {
+        this.timeScaleTween && this.timeScaleTween.kill()
+        this.timeScaleTween = TweenMax.to(this.tl, 1.75, {timeScale: 0.05})
+      })
+
+      $('.person.video').on('click', function(e) {
+        e.stopPropagation()
+
+        tl.pause()
+
+        const personModal = document.getElementById('personModal')
+        const modalContent = personModal.querySelector('.content')
+        const videoSrc = `https://player.vimeo.com/video/${$(this).data('src')}?autoplay=1&color=e82e21&title=0&byline=0&portrait=0`;
+        const video = $('<iframe frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
+
+        video.attr('src', videoSrc)
+        video.attr('autoplay', true)
+
+        $('#ourPeople').addClass('modal-open');
+        $('body').addClass('people-modal-open')
+
+        $(modalContent).append(video)
+
+        if (window.innerWidth >= 700) {
+          $('#ourPeople').one('click', closeVideo)        
+        } else {
+          $('#closePersonModal').one('click', closeVideo)
+        }
+      })
+    }
 
     function closeVideo(e) {
       e.stopPropagation();
@@ -96,60 +164,22 @@ export class OurPeople {
 
       modalContent.innerHTML = ''      
     }
-    
-    $('.person.video').on('mouseenter', () => {
-      this.timeScaleTween && this.timeScaleTween.kill()
-      this.timeScaleTween = TweenMax.to(tl, 1.75, {timeScale: 0})
-    })
-
-    $('.person.video').on('mouseleave', () => {
-      this.timeScaleTween && this.timeScaleTween.kill()
-      this.timeScaleTween = TweenMax.to(tl, 1.75, {timeScale: 0.05})
-    })
-
-    $('.hover-area').on('mouseenter', (e) => {      
-
-      const isScrollingForward = e.currentTarget.id === 'hoverRight'
-      const isReversed = this.tl.reversed()
-
-      if (isScrollingForward && isReversed) {
-        this.tl.reversed(false)
-      } else if( !isScrollingForward && !isReversed ) {
-        this.tl.reverse()
-      }
-
-      this.timeScaleTween && this.timeScaleTween.kill()
-      this.timeScaleTween = TweenMax.to(this.tl, 2, {timeScale: 1})
-    });
-
-    $('.hover-area').on('mouseleave', () => {
-      this.timeScaleTween && this.timeScaleTween.kill()
-      this.timeScaleTween = TweenMax.to(this.tl, 1.75, {timeScale: 0.05})
-    })
-
-    this.tl.add( new TweenMax(this.sliderInner, '5', {
-      left: this.cellWidth * -1,
-      ease: Linear.easeNone,
-      timeScale: this.timeScale
-    }))
-
-    this.tl.timeScale(0.05)
   }
 
 
-  mouseCoords(e) {
-    const xPercent = e.clientX / window.innerWidth * 100
-    const x = xPercent >= 50 ? (xPercent - 50) * -1 : 50 - xPercent
+  // mouseCoords(e) {
+  //   const xPercent = e.clientX / window.innerWidth * 100
+  //   const x = xPercent >= 50 ? (xPercent - 50) * -1 : 50 - xPercent
 
-    const yPercent = e.clientY / window.innerHeight * 100
-    const y = yPercent >= 50 ? (yPercent - 50) * -1 : 50 - yPercent
+  //   const yPercent = e.clientY / window.innerHeight * 100
+  //   const y = yPercent >= 50 ? (yPercent - 50) * -1 : 50 - yPercent
 
-    //returns x and y with positive or negative 50 depending on distance from center
-    return {
-      x: x,
-      y: y
-    }
-  }
+  //   //returns x and y with positive or negative 50 depending on distance from center
+  //   return {
+  //     x: x,
+  //     y: y
+  //   }
+  // }
 
   handleErr(err) {
     console.log(err)
